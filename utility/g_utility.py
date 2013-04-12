3178bedb9c3d70b62609b95378570b213ddd1e7b
baturay <baturay.akaslan@tufts.edu>
2013-04-12 09:16:36 -0400

    Changed the image search a bit. Made title smaller in css so it doesn't go to two lines awkwardly.

--- utility/g_utility.py
+++ utility/g_utility.py
@@ -13,14 +13,36 @@ def gImageSearch(restaurant):
           restaurant.address.state)
   query = query.replace(" ","%20")
 
-  url = ("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+query)
+  url = ("https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + query + 
+         "&key=AIzaSyABwfAPXtUlwllUy7jx1WA4sbKPbDkd9ZE&sensor=false")
+  
+  
+
+  request = urllib2.Request(
+      url, None, {'Referer': 'Noone'})
+  rawResponse = urllib2.urlopen(request)
+  response = simplejson.load(rawResponse)
+
+  if unicode('photos') in response[unicode('results')][0]:
+    photo_reference = response[unicode('results')][0][unicode('photos')][0][unicode('photo_reference')]
+    print photo_reference
+    image_url = ("https://maps.googleapis.com/maps/api/place/photo?photoreference=" + photo_reference +
+              "&sensor=false" +
+              "&maxheight=1600&maxwidth=1600&key=AIzaSyABwfAPXtUlwllUy7jx1WA4sbKPbDkd9ZE")
+    return image_url
+
+  url = ("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+query+
+         "&safe=active&imgsz=small|medium|large|xlarge ")
   
   request = urllib2.Request(
       url, None, {'Referer': 'Noone'})
   rawResponse = urllib2.urlopen(request)
-  
-  # Process the JSON string.
   response = simplejson.load(rawResponse)
+
+
   results = response[unicode('responseData')][unicode('results')]
+
+
+
  
   return results[0][unicode('url')]
