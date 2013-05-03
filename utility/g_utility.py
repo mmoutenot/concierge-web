88a4fd3549cf6d301056c106cf89ca8ece8c50cc
Andrew McGlathery <a.mcglathery@gmail.com>
2013-05-03 13:06:04 -0400

    new google api key

--- utility/g_utility.py
+++ utility/g_utility.py
@@ -6,44 +6,50 @@ from recommendation_item.models import Restaurant, Address
 # user's IP address. Doing so will help distinguish this legitimate
 # server-side traffic from traffic which doesn't come from an end-user.
 
+google_key = 'AIzaSyDh0ollMqMwZ7-jei4IRTFTd5XIXTCNlv8'
+
 def gImageSearch(restaurant):
 
-  query = (restaurant.title + " " + 
+  query = (restaurant.title + " " +
           restaurant.address.city + " " +
           restaurant.address.state)
   query = query.replace(" ","%20")
 
-  url = ("https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + query + 
-         "&key=AIzaSyABwfAPXtUlwllUy7jx1WA4sbKPbDkd9ZE&sensor=false")
-  
-  
+  url = ("https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + query +
+         "&key=" + google_key + "&sensor=false")
+
+
   request = urllib2.Request(
       url, None, {'Referer': 'Noone'})
   rawResponse = urllib2.urlopen(request)
   response = simplejson.load(rawResponse)
-
+  print response
   if unicode('results') in response and len(response[unicode('results')]) > 0:
     if unicode('photos') in response[unicode('results')][0]:
       photo_reference = response[unicode('results')][0][unicode('photos')][0][unicode('photo_reference')]
       print photo_reference
       image_url = ("https://maps.googleapis.com/maps/api/place/photo?photoreference=" + photo_reference +
                 "&sensor=false" +
-                "&maxheight=1600&maxwidth=1600&key=AIzaSyABwfAPXtUlwllUy7jx1WA4sbKPbDkd9ZE")
+                "&maxheight=1600&maxwidth=1600&key=" + google_key)
       return image_url
 
   url = ("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+query+
          "&safe=active")
-  
+
   request = urllib2.Request(
       url, None, {'Referer': 'Noone'})
   rawResponse = urllib2.urlopen(request)
   response = simplejson.load(rawResponse)
+  print response
 
 
 
-  results = response[unicode('responseData')][unicode('results')]
+  if unicode('results') in response and len(response[unicode('results')]) > 0:
+    results = response[unicode('responseData')][unicode('results')]
+  else:
+    return ''
 
 
 
- 
+
   return results[0][unicode('url')]
