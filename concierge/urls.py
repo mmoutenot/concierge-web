1a7bbff236ec6abd8156857182d22ffbd527988a
Marshall <mmoutenot@gmail.com>
2013-05-02 20:15:21 -0400

    fix logout route to use contrib.auth's version. Maybe this helps?

--- concierge/urls.py
+++ concierge/urls.py
@@ -1,13 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+
 admin.autodiscover()
 
-urlpatterns = patterns('concierge.views',
+urlpatterns = patterns('',
+    (r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+)
+
+urlpatterns += patterns('concierge.views',
     (r'^admin/', include(admin.site.urls)),
     url(r'', include('user_profile.urls')),
     url(r'', include('goto.urls')),
-    url(r'^logout/$', 'logout_user'),
     url(r'^$', 'index', name='index'),
+
 )
 
 urlpatterns += patterns('recommendation_item.views',
